8e281d059c75f50c4bcbc22f32f2f545da23d18a
Carlos Caldas <ccaldas@gmail.com>
2016-07-11 04:08:00 -0300

    Extra-Long-Factorial - medium

---
+++ HackerRank/csharp/Extra-Long-Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Extra_Long_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            BigInteger big = new BigInteger(n--);
+            while (n > 1)
+            {
+                big *= n--;
+            }
+            Console.WriteLine(big);
+        }
+    }
+}
